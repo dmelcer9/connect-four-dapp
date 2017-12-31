ea757444feb6b2d2c1c6dbfc0b69219dfc67aa5b
dmelcer9 <dmelcer9@gmail.com>
2017-12-31 17:47:08 -0500

    Actually connects to metamask now

--- README.md
+++ README.md
@@ -11,9 +11,7 @@ To test changes:
  - Start ganache
    - The GUI for ganache is a bit buggy, so use the command-line ganache instead
    - `ganache-cli -p 7545 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"`
- - To test the smart contract
+ - To test the smart contract itself
    - `npm run test`
- - To test changes and redeploy the smart contract to ganache:
-   - `npm run dw`
- - To test changes to the webpage
-   - `npm run build` _or_ `webpack`
+ - To deploy the contract to ganache and run a dev server:
+   - `npm run dev`
--- app/script/main.js
+++ app/script/main.js
@@ -1,4 +1,4 @@
-import Web3 from 'web3'
+var Web3 = require("web3");
 
 import contract from 'truffle-contract'
 
@@ -8,12 +8,29 @@ const ConnectFour = contract(con4_artifacts);
 class ConnectFourApp{
 
   constructor(web3Inst){
-    ConnectFour.setProvider(web3.currentProvider);
+    this.web3i = web3Inst;
+    ConnectFour.setProvider(this.web3i.currentProvider);
     ConnectFour.deployed()
-      .then(function(deployed){
+      .then(deployed => {
         console.log("Connected to contract")
         this.c4inst = deployed;
-      }).catch(displayError);
+      }).then(()=>this.setUpDOM())
+      .catch(this.displayError);
+  }
+
+  setUpDOM(){
+
+    var t = this.c4inst;
+    document.getElementById("getInfo").addEventListener("click", async ()=>{
+      var acc = (await this.web3i.eth.getAccounts())[0];
+      console.log(this.web3i);
+      console.log(acc);
+      await t.createNewGame({from:acc,value:"10000000"});
+      console.log("Test");
+      var num = document.getElementById("idnum").value;
+      var arr = await t.getPlayerOne.call(num);
+
+    })
   }
 
   displayError(error){
@@ -21,19 +38,17 @@ class ConnectFourApp{
   }
 }
 
-
-
-
 window.addEventListener('load', function() {
   var web3used;
 
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     // Use Mist/MetaMask's provider
+    console.log("Using mist/metamask");
     web3used = new Web3(web3.currentProvider);
   } else {
-    console.log("No web3 detected");
-    web3used = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+    //TODO Improve this error message
+    console.error("No web3 detected");
   }
 
   new ConnectFourApp(web3used);
